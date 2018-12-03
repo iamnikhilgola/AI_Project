9b42583b383a6eddaabc283da616b25648d61a6e
yogesh18140 <43813000+yogesh18140@users.noreply.github.com>
2018-12-03 17:44:57 +0530

    Code updated

--- spaceAssaulter/spaceAssaulter.py
+++ spaceAssaulter/spaceAssaulter.py
@@ -24,7 +24,7 @@ PRIGHT = False
 NOMOVE = True
 
 #Bullet speed
-BSPEED = 40
+BSPEED = 10
 SHOOT = False
 
 #Color Vairables 
@@ -37,15 +37,20 @@ BLUE = (0,0,255)
 #Agent List
 AGENTS = []
 class Bullet(pg.sprite.Sprite):
-    def __init__(self,centerx,centery,speed):
+    def __init__(self,x,y):
         pg.sprite.Sprite.__init__(self)
-        self.centerx = centerx
-        self.centery = centery
-        self.image = pg.Surface([2,3])
-        self.speed = speed
+        self.image = pg.Surface((10,20))
+        self.speed = BSPEED
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-    
+        self.rect.y = y
+        self.rect.x = x
+        
+    def update(self):
+        self.rect.x = self.rect.x
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill() 
        
         
 class Rocket(pg.sprite.Sprite):
@@ -62,16 +67,20 @@ class Rocket(pg.sprite.Sprite):
         self.rect.centerx = self.centerx
         self.rect.centery = self.bottom-10
     
+    
     def move(self):
         if (PLEFT == True) and (self.rect.x>5):
             self.rect.x -=self.speed
-        elif PRIGHT == True and self.rect.x < WINDOW_WIDTH -20:
+        elif PRIGHT == True and self.rect.x < WINDOW_WIDTH -25:
             self.rect.x += self.speed
         else :
             pass
         
     def shoot(self):
-        pass
+        newBullet =Bullet(self.rect.x,self.rect.y)
+        newBullet.update()
+        return newBullet
+        
             
 class Enemy(pg.sprite.Sprite):
     def __init__(self,centerx,centery,probMoving):
@@ -86,6 +95,7 @@ class Enemy(pg.sprite.Sprite):
         self.speed = randint(1,5)
         self.rect.centerx = self.centerx
         self.rect.centery = self.centery
+        self.time = time.time()
     
     def move(self):
         if self.direction == UP_LEFT:
@@ -121,7 +131,7 @@ class Enemy(pg.sprite.Sprite):
         else:
             if self.rect.y<0:
                 self.direction = random.choice([3,4,5,6])
-            elif self.rect.x>WINDOW_WIDTH-20:
+            elif self.rect.x>WINDOW_WIDTH-45:
                 self.direction = random.choice([1,4,5])
             elif self.rect.y>WINDOW_HEIGHT-100:
                 self.direction = random.choice([1,2,5,6])
@@ -129,20 +139,43 @@ class Enemy(pg.sprite.Sprite):
                 self.direction = random.choice([2,3,6])
             else: 
                 pass
-            
+
+def getMaxTime(Array):
+    timeArray=[x.time for x in Array]
+    time1 = max(timeArray)
+    timeArray.remove(time1)
+    time2 = max(timeArray)
+    return time1,time2
+
+def EnemyBreeding(enemy1,enemy2,maxLiver1,maxLiver2,gameWindow):
+    newEnemy=[]
+    if pg.sprite.collide_rect(enemy1,enemy2):
+        if enemy1.time==maxLiver1 or enemy2.time==maxLiver1:
+            for i in range(2):
+                newEnemy.append(Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,i))
+        elif enemy1.time == maxLiver2 or enemy2.time == maxLiver2:
+            for i in range(2):
+                newEnemy.append(Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,i))
+    return(newEnemy)            
+
+
 def playGame(gameWindow,surface_rect,clock):
     rocket = Rocket(gameWindow.get_rect().bottom,gameWindow.get_rect().centerx,gameWindow.get_rect().centery)
     
-    probability =np.linspace(0,1,20)
+    probability =np.linspace(0,1,4)
     EnemyArray =[]
     for i in probability:
         EnemyArray.append(Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,i))
     #e1 = Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,0.010)
     #e2 = Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,0.200)
     #e3 = Enemy(gameWindow.get_rect().centerx,gameWindow.get_rect().centery,0.005)
-    sprites = pg.sprite.RenderPlain(rocket,tuple(EnemyArray))#e1,e2,e3)
+    #sprites = pg.sprite.RenderPlain(rocket,tuple(EnemyArray))#e1,e2,e3)
     counter = 0
+    bullets=[]
+    Score= 0
+    score_font = pg.font.SysFont("Helvetica",100)
     while True:
+        sprites = pg.sprite.RenderPlain(rocket,tuple(EnemyArray),tuple(bullets))
         clock.tick(60)
         for event in pg.event.get():
             global PLEFT
@@ -160,6 +193,11 @@ def playGame(gameWindow,surface_rect,clock):
                     PLEFT = False
                     PRIGHT = True
                     NOMOVE = False
+                if event.key == K_SPACE:
+                    bullet=rocket.shoot()
+                    #bullet.update()
+                    bullets.append(bullet)
+            
             if event.type == KEYUP:
                 if event.key == K_LEFT:
                     PLEFT = False
@@ -169,12 +207,40 @@ def playGame(gameWindow,surface_rect,clock):
                     PLEFT = False
                     PRIGHT = False
                     NOMOVE = True
+                #if event.key == K_UP:
                     
+        for fire in bullets:
+            fire.update()
+            
         gameWindow.fill(BLACK)
         sprites.draw(gameWindow)
         for enemy in EnemyArray:
             enemy.move()
             enemy.changeDirection(counter)
+        
+        if(counter%8==0):
+            for enemy in EnemyArray:
+                for secenemy in EnemyArray:
+                    if (enemy != secenemy and len(EnemyArray)<=100):
+                        maxtime1,maxtime2= getMaxTime(EnemyArray)
+                        newEnemy=EnemyBreeding(enemy,secenemy,maxtime1,maxtime2,gameWindow)
+            EnemyArray+=newEnemy
+        
+        for enemy in EnemyArray:
+            for fire in bullets:
+                if pg.sprite.collide_rect(enemy,fire):
+                    enemy.kill()
+                    fire.kill()
+                    Score = Score+10
+                    EnemyArray.remove(enemy)
+                    bullets.remove(fire)
+                    break
+        scoreBoard = score_font.render(str(Score)+True,WHITE,BLACK)
+        scoreBoard_rect = scoreBoard.get_rect()
+        scoreBoard_rect.centerx = surface_rect.centerx
+        scoreBoard_rect.y = 10
+        #mainWindow.blit(scoreBoard,scoreBoard_rect)
+        #print(EnemyArray)
 # =============================================================================
 #         e1.move()
 #         e1.changeDirection(counter)
@@ -188,6 +254,8 @@ def playGame(gameWindow,surface_rect,clock):
         counter +=1
         if counter ==1000000:
             counter =0
+        if(len(EnemyArray)>100):
+            sys.exit()
 
                 
 def getClock():
